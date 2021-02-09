28885a0110cf6e5064f31bb2889c721f106ec824
unknown <xandemagnocorreia@gmail.com>
2021-02-08 22:05:19 -0300

    login box and background

--- src/Views/Login/Login.js
+++ src/Views/Login/Login.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 import './Login.css'
+import imagemLogin from '../../images/imagem-loginPage.png'
 
 export default function loginPage() {
 
     return (
         <>
-            <Grid className="loginPage">
+            <Grid container className="loginPage">
+                <Grid className="elipse1"></Grid>
+                <Grid className="elipse2"></Grid>
+                <Grid className="elipse3"></Grid>
+                <Grid className="elipse4"></Grid>
+                <Grid className="elipse5"></Grid>
+                <Grid className="elipse6"></Grid>
+                <Grid className="elipse7"></Grid>
                 <Grid className="loginBox">
-                    <h1>login box dos cria</h1>
+                    <Grid>
+                        <img src={imagemLogin} />
+                    </Grid>
+                    <Grid className="loginForm">
+                        <h1>Login</h1>
+                    </Grid>
                 </Grid>
             </Grid>
         </>
